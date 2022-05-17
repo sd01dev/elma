2ee5cf57549f96427b87d17dc7bf375b8225f1fc
sd01dev <sanearo@mail.ru>
2022-05-17 17:10:22 +0300

    Implemented worker pool pattern

--- main.go
+++ main.go
@@ -4,30 +4,46 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 )
 
 const (
-	maxGoroutines = 5
-	intendedWord  = "Go"
-	golangURL     = "https://golang.org"
-	goWiki        = "https://en.wikipedia.org/wiki/Go_(programming_language)"
+	workerLimit  = 5
+	intendedWord = "Go"
+	golangURL    = "https://golang.org"
+	goWiki       = "https://en.wikipedia.org/wiki/Go_(programming_language)"
 )
 
+type site struct {
+	url string
+}
+
+type matches struct {
+	amount int
+}
+
 func main() {
-	ch := make(chan int, maxGoroutines)
-	sum := 0
+	jobs := make(chan site, workerLimit)
+	results := make(chan matches, workerLimit)
 	urlList := []string{goWiki, golangURL, goWiki, golangURL, goWiki, golangURL}
+	sum := 0
 
-	for _, value := range urlList {
-		go countIntendedWordNumber(ch, value, intendedWord)
+	for w := 1; w <= workerLimit; w++ {
+		go countIntendedWordNumber(jobs, results, intendedWord)
 	}
 
-	for i := len(urlList); i != 0; i-- {
-		sum += <-ch
+	for _, currentUrl := range urlList {
+		jobs <- site{url: currentUrl}
 	}
-	fmt.Printf("Total: %d", sum)
+	close(jobs)
+
+	for i := 1; i <= len(urlList); i++ {
+		amount := <-results
+		sum += amount.amount
+	}
+	log.Printf("Total: %d", sum)
 }
 
 func wordCount(text, intendedWord string) int {
@@ -42,7 +58,7 @@ func wordCount(text, intendedWord string) int {
 func getData(url string) (string, error) {
 	req, err := http.Get(url)
 	if err != nil {
-		return "", fmt.Errorf("get URL error: %w", err)
+		return "", fmt.Errorf("get url error: %w", err)
 	}
 
 	defer func(Body io.ReadCloser) {
@@ -61,12 +77,14 @@ func getData(url string) (string, error) {
 
 // Counts only intended word covered with spaces. Example: " Go ".
 // If needed count with special symbols before\after desired word, should use regexp.
-func countIntendedWordNumber(ch chan int, url, intendedWord string) {
-	data, err := getData(url)
-	if err != nil {
-		return
+func countIntendedWordNumber(jobs <-chan site, results chan<- matches, intendedWord string) {
+	for site := range jobs {
+		data, err := getData(site.url)
+		if err != nil {
+			return
+		}
+		repeatedWord := wordCount(data, intendedWord)
+		log.Printf("Count for %s: %v\n", site.url, repeatedWord)
+		results <- matches{amount: repeatedWord}
 	}
-	matches := wordCount(data, intendedWord)
-	fmt.Printf("Count for %s: %v\n", url, matches)
-	ch <- matches
 }
